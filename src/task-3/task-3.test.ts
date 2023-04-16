ef1472caffbf4d467a2b6938e8dd1cca349927b6
Arek <arek.stachyra41@gmail.com>
2023-04-16 21:37:18 +0200

    test: update mock data in most valuable cart test

--- src/task-3/task-3.test.ts
+++ src/task-3/task-3.test.ts
@@ -3,66 +3,78 @@ import { describe, test, expect } from 'vitest';
 import type { Cart, Product, User } from '../api-types';
 
 describe('getMostValuableCart', () => {
-	// test('should return the most valuable cart given an array of carts', () => {
-	// 	const carts = [
-	// 		{
-	// 			id: 1,
-	// 			userId: 1,
-	// 			date: '2020-03-02T00:00:00.000Z',
-	// 			products: [
-	// 				{
-	// 					productId: 1,
-	// 					quantity: 4,
-	// 				},
-	// 				{
-	// 					productId: 2,
-	// 					quantity: 1,
-	// 				},
-	// 				{
-	// 					productId: 3,
-	// 					quantity: 6,
-	// 				},
-	// 			],
-	// 			__v: 0,
-	// 		},
-	// 		{
-	// 			id: 2,
-	// 			userId: 1,
-	// 			date: '2020-01-02T00:00:00.000Z',
-	// 			products: [
-	// 				{
-	// 					productId: 2,
-	// 					quantity: 4,
-	// 				},
-	// 				{
-	// 					productId: 1,
-	// 					quantity: 10,
-	// 				},
-	// 				{
-	// 					productId: 5,
-	// 					quantity: 2,
-	// 				},
-	// 			],
-	// 			__v: 0,
-	// 		},
-	// 		{
-	// 			id: 6,
-	// 			userId: 4,
-	// 			date: '2020-03-01T00:00:00.000Z',
-	// 			products: [
-	// 				{
-	// 					productId: 10,
-	// 					quantity: 2,
-	// 				},
-	// 				{
-	// 					productId: 12,
-	// 					quantity: 3,
-	// 				},
-	// 			],
-	// 			__v: 0,
-	// 		},
-	// 	];
-	// });
+	test('should return the most valuable cart given an array of carts', () => {
+		const carts = [
+			{
+				id: 1,
+				userId: 1,
+				date: '2020-03-02T00:00:00.000Z',
+				products: [
+					{
+						productId: 1,
+						quantity: 4,
+					},
+					{
+						productId: 2,
+						quantity: 1,
+					},
+					{
+						productId: 3,
+						quantity: 6,
+					},
+				],
+				__v: 0,
+			},
+			{
+				id: 2,
+				userId: 1,
+				date: '2020-01-02T00:00:00.000Z',
+				products: [
+					{
+						productId: 2,
+						quantity: 4,
+					},
+					{
+						productId: 1,
+						quantity: 10,
+					},
+					{
+						productId: 5,
+						quantity: 2,
+					},
+				],
+				__v: 0,
+			},
+			{
+				id: 6,
+				userId: 4,
+				date: '2020-03-01T00:00:00.000Z',
+				products: [
+					{
+						productId: 10,
+						quantity: 2,
+					},
+					{
+						productId: 12,
+						quantity: 3,
+					},
+				],
+				__v: 0,
+			},
+			{
+				id: 7,
+				userId: 8,
+				date: '2020-03-01T00:00:00.000Z',
+				products: [
+					{
+						productId: 18,
+						quantity: 1,
+					},
+				],
+				__v: 0,
+			},
+		];
+	});
 	test('should an object with initial values given an empty array of carts', () => {
 		const carts: Cart[] = [];
 		const products: Product[] = [];
