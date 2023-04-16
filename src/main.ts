fff115a58d677a03ee67fd97fcfa413b8e655865
Arek <arek.stachyra41@gmail.com>
2023-04-16 21:36:21 +0200

    feat: add code to manage simple caching

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,48 @@
+import './reset.css';
 import './style.css';
+import fetchData from './task-1/task-1';
+import type { Product, User, Cart } from './api-types';
+import getCategoriesPrices from './task-2/task-2';
+import getMostValuableCart from './task-3/task-3';
+import getFurthestUsers from './task-4/task-4';
+
+let users: User[] = [];
+let products: Product[] = [];
+let carts: Cart[] = [];
+let dataFetched = false;
+
+// I know this caching is not the best but at the same time I don't really need anything
+// more complicated than that. I just wanted something so that I don't fetch the data with
+// every button click.
+function reportCacheStatus() {
+	const cacheStatus = document.querySelector<HTMLDivElement>('#cache-status');
+	if (!dataFetched) {
+		cacheStatus.textContent = 'API data is not cached';
+	} else {
+		cacheStatus.textContent = 'API data is cached';
+	}
+}
+
+async function fetchDataAndSetFlag() {
+	dataFetched = false;
+	reportCacheStatus();
+	const loadingText = document.querySelector<HTMLDivElement>('#loading');
+	if (loadingText !== null) {
+		loadingText.textContent = 'Loading data...';
+	}
+
+	users = await fetchData<User[]>('https://fakestoreapi.com/users');
+	products = await fetchData<Product[]>('https://fakestoreapi.com/products');
+	carts = await fetchData<Cart[]>(
+		'https://fakestoreapi.com/carts/?startdate=2000-01-01&enddate=2023-04-07'
+	);
+
+	dataFetched = true;
+	reportCacheStatus();
+	if (loadingText !== null) {
+		loadingText.textContent = '';
+	}
+}
 
 function getErrorMessage(error: unknown) {
 	if (error instanceof Error) return error.message;
