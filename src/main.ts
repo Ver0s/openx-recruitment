2f393d223e69ae4d4e875c6a6ff3f842ae44bd54
Arek <arek.stachyra41@gmail.com>
2023-04-16 21:35:23 +0200

    feat: add event listeners for buttons

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,28 @@ async function getData<T>(url: string) {
 		throw new Error(getErrorMessage(err));
 	}
 }
+
+const task1Btn = document.querySelector<HTMLButtonElement>('#task-1');
+task1Btn?.addEventListener('click', () => {
+	void taskManager('task1');
+});
+
+const task2Btn = document.querySelector<HTMLButtonElement>('#task-2');
+task2Btn?.addEventListener('click', () => {
+	void taskManager('task2');
+});
+
+const task3Btn = document.querySelector<HTMLButtonElement>('#task-3');
+task3Btn?.addEventListener('click', () => {
+	void taskManager('task3');
+});
+
+const task4Btn = document.querySelector<HTMLButtonElement>('#task-4');
+task4Btn?.addEventListener('click', () => {
+	void taskManager('task4');
+});
+
+const refetchBtn = document.querySelector<HTMLButtonElement>('#refetch');
+refetchBtn?.addEventListener('click', () => {
+	void fetchDataAndSetFlag();
+});
